96c9ad9feb2da7348b3ba7e7e9f81c17e8c4ea47
Marcus Brandão <marcusergue@gmail.com>
2026-03-09 11:13:04 -0300

    refactor: separa componente Home da estrutura de rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SchedulePage from "./pages/SchedulePage";
 
+function Home() {
+
+  return (
+
+    <div style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      height:"100vh",
+      fontSize:"40px",
+      fontWeight:600,
+      color:"#ffffff"
+    }}>
+      Agendify
+    </div>
+
+  );
+
+}
+
 function App() {
 
   return (
+
     <BrowserRouter>
 
       <Routes>
 
-        <Route path="/" element={<div style={{color:"white", textAlign:"center", marginTop:"100px"}}>Agendify</div>} />
+        <Route path="/" element={<Home />} />
 
         <Route path="/:slug" element={<SchedulePage />} />
 
       </Routes>
 
     </BrowserRouter>
-  )
+
+  );
 
 }
 
-export default App
+export default App;
